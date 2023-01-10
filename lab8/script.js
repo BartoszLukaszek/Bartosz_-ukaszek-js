31a72fa6feb4b4744f6e42f737649cf0c7a988fd
BartoszLukaszek <barti120@onet.pl>
2023-01-10 20:02:30 +0100

    lab8

--- lab8/script.js
+++ lab8/script.js
@@ -1,57 +1,71 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const ballRadius = 20; // promień kulki
-let x = canvas.width / 2; // początkowa pozycja x kulki
-let y = canvas.height - 30; // początkowa pozycja y kulki
-let dx = 2; // prędkość pozioma
-let dy = -2; // prędkość pionowa
-const balls = []; // tablica zawierająca kulki
-let yThreshold = 0.2 * canvas.width; // próg odległości pomiędzy kulkami
+    const canvas = document.querySelector('canvas');
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
-// Klasa reprezentująca pojedynczą kulkę
-class Ball {
-  constructor(x, y, dx, dy) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-  }
+      canvas.width = width;
+      canvas.height = height;
 
-  // Rysowanie kulki na canvasie
-  draw() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-  }
+      const ctx = canvas.getContext('2d');
+      class Ball {
+        constructor(x, y, velx, vely, size, color) {
+          this.x = x;
+          this.y = y; 
+          this.velx = velx; 
+          this.vely = vely; 
+          this.size = size; 
+          this.color = color; 
+        }
 
-  // Aktualizowanie pozycji kulki
-  update() {
-    // Odbijanie się od krawędzi canvasu
-    if (this.x + this.dx > canvas.width - ballRadius || this.x + this.dx < ballRadius) {
-      this.dx = -this.dx;
-    }
-    if (this.y + this.dy > canvas.height - ballRadius || this.y + this.dy < ballRadius) {
-      this.dy = -this.dy;
-    }
+        drawBall() {
+          ctx.beginPath(); 
+          ctx.fillStyle = this.color; 
 
-    this.x += this.dx;
-    this.y += this.dy;
+          ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+          ctx.fill(); 
+        }
+        updateBall() {
+          if (this.x + this.size >= width || this.x - this.size <= 0) {
+            this.velx = -this.velx;
+          }
 
-    // Rysowanie linii pomiędzy kulkami, jeśli odległość jest mniejsza niż yThreshold
-    for (let i = 0; i < balls.length; i++) {
-      if (this !== balls[i]) {
-        let distance = Math.sqrt(Math.pow(this.x - balls[i].x, 2) + Math.pow(this.y - balls[i].y, 2));
-        if (distance < yThreshold) {
-          ctx.beginPath();
-          ctx.moveTo(this.x, this.y);
-          ctx.lineTo(balls[i].x, balls[i].y);
-          ctx.strokeStyle = "#0095DD";
-          ctx.stroke();
+          if (this.y + this.size >= height || this.y - this.size <= 0) {
+            this.vely = -this.vely;
+          }
+
+          this.x += this.velx;
+          this.y += this.vely;
         }
       }
-    }
-  }
-}
+      function random(min, max) {
+        const num = Math.floor(Math.random() * (max - min + 1)) + min;
+        return num;
+      }
+      const balls = [];
 
+      while (balls.length < 25) {
+        let size = random(10, 20);
+
+        const ball = new Ball(
+          random(size, width - size),
+          random(size, height - size),
+          random(-7, 7),
+          random(-7, 7),
+          size,
+          `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`
+        );
+
+        balls.push(ball);
+      }
+      function loop() {
+        
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+        ctx.fillRect(0, 0, width, height);
+
+        for (let i = 0; i < balls.length; i++) {
+          balls[i].drawBall();
+          balls[i].updateBall();
+        }
+        requestAnimationFrame(loop);
+      }
+      loop();
+ 
