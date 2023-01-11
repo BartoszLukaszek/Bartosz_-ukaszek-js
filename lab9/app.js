a1c3afd7b73432083087fb63fcc43fa3a9927654
BartoszLukaszek <barti120@onet.pl>
2023-01-11 16:55:21 +0100

    Dodanie temperatury/Godziny

--- lab9/app.js
+++ lab9/app.js
@@ -1,16 +1,13 @@
-/** @format */
-
 const cityForm = document.getElementById("city-form");
 const cityInput = document.getElementById("city");
 const cityName = document.getElementById("city-name");
 const weatherState = document.getElementById("weather-state");
-
 const apiKey = "&APPID=86b519a53292e52d2fe7f4562db93281";
-
 let apiRequest = new XMLHttpRequest();
+const currentTime = new Date().toLocaleTimeString();
 
-cityForm.addEventListener("submit", $event => {
-	$event.preventDefault();
+cityForm.addEventListener("submit", event => {
+	event.preventDefault();
 	const chosenCity = cityInput.value;
 
 	apiRequest.open(
@@ -21,20 +18,23 @@ cityForm.addEventListener("submit", $event => {
 	);
 	apiRequest.send();
 });
+
 apiRequest.onreadystatechange = () => {
 	if (apiRequest.readyState === 4) {
 		if (apiRequest.status === 404) {
 			weatherState.textContent = "City not found";
 		}
-
 		const response = JSON.parse(apiRequest.response);
-
-		$cityName = cityName.textContent = response.name;
+		cityName.textContent = response.name;
+        const temperatureKelvin = response.main.temp;
+        const temperatureCelsius = Math.round(temperatureKelvin  - 273.15);
+        const temperatureFahrenheit = (temperatureKelvin - 273.15) * 9/5 + 32;
 		weatherState.textContent =
 			"The weather in " +
-			$cityName +
+			response.name +
 			" is " +
 			response.weather[0].main +
-			".";
+			" at " + currentTime + " temperature:"+ temperatureCelsius + " C " ;
+
 	}
 };
